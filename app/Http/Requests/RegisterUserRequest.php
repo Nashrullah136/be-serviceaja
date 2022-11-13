b372c5dfef7a4c518bca5b0878746736e75e3502
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-11-13 16:09:23 +0700

    Make form request for each model to validate the request

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:App\Models\User,email'],
+            'phone_number' => ['required', 'numeric', 'digits_between:10,13'],
+            'password' => ['required', 'string', 'gte:8'],
+        ];
+    }
+}
